8e9f53e71b6c5cd325d6deda9c95894d33f3cf0c
Nykyta Fynazhyn <nikita.finazhin@gmail.com>
2019-11-16 07:53:10 +0200

    first commit

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace GameLoop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._openGLControl = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.SuspendLayout();
+            // 
+            // simpleOpenGlControl1
+            // 
+            this._openGLControl.AccumBits = ((byte)(0));
+            this._openGLControl.AutoCheckErrors = false;
+            this._openGLControl.AutoFinish = false;
+            this._openGLControl.AutoMakeCurrent = true;
+            this._openGLControl.AutoSwapBuffers = true;
+            this._openGLControl.BackColor = System.Drawing.Color.Black;
+            this._openGLControl.ColorBits = ((byte)(32));
+            this._openGLControl.DepthBits = ((byte)(16));
+            this._openGLControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._openGLControl.Location = new System.Drawing.Point(0, 0);
+            this._openGLControl.Name = "simpleOpenGlControl1";
+            this._openGLControl.Size = new System.Drawing.Size(800, 450);
+            this._openGLControl.StencilBits = ((byte)(0));
+            this._openGLControl.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this._openGLControl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl _openGLControl;
+
+        
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Tao.OpenGl;
+
+namespace GameLoop
+{
+    public partial class Form1 : Form
+    {
+        FastLoop _fastLoop;
+        bool _fullscreen = false;
+
+
+        /* Весь закоментированный код ниже можно удалить, 
+         * он нужен только чтоб вспомнить, как работает программа*/
+
+        public Form1()
+        {
+            /* Создает gameloop внутри формы, для обработки элементов и их гладкой анимации */
+            //_fastLoop = new FastLoop(GameLoop);   
+
+            //InitializeComponent();
+            //_openGLControl.InitializeContexts();
+
+
+            //if (_fullscreen)
+            //{
+            //    FormBorderStyle = FormBorderStyle.None;
+            //    WindowState = FormWindowState.Maximized;
+            //}
+        }
+
+        void GameLoop(double elapsedTime)
+        {
+            //Gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+            //Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+
+            //Gl.glRotated(elapsedTime * 50, 0, 1, 0);
+            //Gl.glBegin(Gl.GL_TRIANGLE_STRIP);
+            //{
+            //    Random r = new Random();
+            //    Gl.glColor3d(1, 0, 0);
+            //    Gl.glVertex3d(-0.5, 0, 0);
+            //    Gl.glColor3d(0, 1, 0);
+            //    Gl.glVertex3d(0.5, 0, 0);
+            //    Gl.glColor3d(0, 0, 1);
+            //    Gl.glVertex3d(0, 0.5, 0);
+            //}
+            //Gl.glEnd();
+
+            //Gl.glFinish();
+            //_openGLControl.Refresh();
+            
+        }
+    }
+
+}
